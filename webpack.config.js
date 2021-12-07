9589c5d10f450d5bc9792993b4949039e7381777
nikhilpandey-dev <nikhilpandey4@gmail.com>
2021-12-07 07:22:23 +0530

    Added plugins and image loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,37 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/client/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        clean: true
+    },
+    stats: 'normal',
+    module: {
+        rules: [
+            {
+                test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/i,
+                type: "asset/resource"
+
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
+}
+
