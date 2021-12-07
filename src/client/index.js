d2e05ea66770f58975b38e78dab29eef3aebcd23
nikhilpandey-dev <nikhilpandey4@gmail.com>
2021-12-07 20:09:37 +0530

    Added the original avenue image to the html and now it remains to integrate css.

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,1 @@
-import mainImage from '/src//assets/images/cafe-6553202.jpeg'
-alert("I exist!");
-
-
-const img = document.getElementsByClassName("hero-img");
-img.src = mainImage;
+console.log('Hurray, did image loading!');
